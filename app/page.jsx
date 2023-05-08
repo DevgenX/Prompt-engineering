7d873ec8fd8e6582354a939dd0659cc91bc3a213
devgenx <sebastian110420@gmail.com>
2023-05-08 02:57:01 -0700

    initial homepage design and responsiveness

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,18 @@
-const page = () => {
-  return <div>page</div>;
+import Feed from "@components/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        The best
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptify lets you create and use AI prompts for modern web interaction
+      </p>
+      <Feed />
+    </section>
+  );
 };
-export default page;
+export default Home;
