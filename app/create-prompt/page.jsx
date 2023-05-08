8601dfdabbdea873a76e92a01d70557dbb5141da
devgenx <sebastian110420@gmail.com>
2023-05-08 08:29:14 -0700

    added get request for prompts and rendered on home page

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
+
+const Prompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [isSubmitting, setIsSubmitted] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const handleCreatePrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsSubmitted(false);
+    }
+  };
+
+  return (
+    <div>
+      <Form
+        type="Create"
+        post={post}
+        setPost={setPost}
+        submitting={isSubmitting}
+        handleSubmit={handleCreatePrompt}
+      />
+    </div>
+  );
+};
+export default Prompt;
